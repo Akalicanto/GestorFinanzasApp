abd6bb89654366f1141c81060300089119d145de
CARLOSPC\carlo <carlose.torresc@gmail.com>
2025-09-07 18:48:23 +0200

    Db y task automatico

--- GestorFinanzasApp.Server/Program.cs
+++ GestorFinanzasApp.Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using GestorFinanzasApp.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<GestorFinanzasDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<GestorFinanzasDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
